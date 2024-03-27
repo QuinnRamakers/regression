d0b28ee2403bba4b0b74e8e7ad247a0070888737
QuinnRamakers <quinnmramakers@gmail.com>
2024-03-27 12:28:44 +0100

    ['p']

--- R/GLS.R
+++ R/GLS.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param y Response
+#' @param X model matrix
+#' @param sigma Uninverted covariance matrix
+#'
+#' @return GLS estimator of beta
+#' @export
+#'
+#' @examples y=c(0.978,1.08) x=(0.383,0.45) sigma=diag(2)
 GLS<-function(y,X,sigma){
   return(solve(t(X)%*%solve(sigma)%*%X,t(X)%*%solve(sigma)%*%y))
 }
--- R/ols.R
+++ R/ols.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param y  Response
+#' @param X Model Matrix
+#'
+#' @return OLS estimator of beta
+#' @export
+#'
+#' @examples y=c(0.978,1.08) x=(0.383,0.45)
 OLS<-function(y,X){
   return(solve(t(X)%*%X,t(X)%*%y))
 }
